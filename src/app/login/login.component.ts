c6c0e008b2a55c9d7fd2b68026baa0c2ea3bb1fe
Evison <35538753+evisonpacheco@users.noreply.github.com>
2022-07-04 15:15:56 -0300

    update login: validação do formulario adicionado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-login',
@@ -6,4 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['../../styles.css']
 })
 
-export class LoginComponent { }
+export class LoginComponent {
+
+  constructor(private router: Router) { }
+
+  validateLogin(){
+    let email = (document.getElementById("email-login") as HTMLInputElement).value;
+    let cpf = (document.getElementById("cpf-login") as HTMLInputElement).value;
+    let pis = (document.getElementById("pis-login") as HTMLInputElement).value;
+    let password = (document.getElementById("password-login") as HTMLInputElement).value;
+
+    if ((email == "") && (cpf == "") && (pis == "")) {
+      alert("Escolha pelo menos uma forma de Login!");
+    } else if (password == "") {
+      alert("Preencha o campo senha!")
+    } else {
+      this.router.navigate(['/usuario']);
+    }
+  }
+}
