eb571ae78132fa444827c8a98c18a440e8e5cbc2
Evison <35538753+evisonpacheco@users.noreply.github.com>
2022-07-04 15:56:26 -0300

    update register: validação adicionada

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,5 @@
-import { UserInterface } from './../../assets/interfaces/user.interface';
 import { Component } from '@angular/core';
-import { UserService } from 'src/assets/services/user/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,15 +9,30 @@ import { UserService } from 'src/assets/services/user/user.service';
 
 export class RegisterComponent {
 
-  constructor(private UserService: UserService) { }
+  constructor(private router: Router) {}
 
-    validate(){
+
+    validateRegistration(){
+      let userName = (document.getElementById("name") as HTMLInputElement).value;
+      let email = (document.getElementById("email") as HTMLInputElement).value;
+      let cpf = (document.getElementById("cpf") as HTMLInputElement).value;
+      let pis = (document.getElementById("pis") as HTMLInputElement).value;
       let password = (document.getElementById("password") as HTMLInputElement).value;
       let passwordConfirm = (document.getElementById("password-confirm") as HTMLInputElement).value;
+      let country = (document.getElementById("country") as HTMLInputElement).value;
+      let state = (document.getElementById("state") as HTMLInputElement).value;
+      let city = (document.getElementById("city") as HTMLInputElement).value;
+      let number = (document.getElementById("number") as HTMLInputElement).value;
+      let complement = (document.getElementById("complement") as HTMLInputElement).value;
 
-    console.log(password, passwordConfirm)
-      if (password != passwordConfirm) {
+      if ((userName == "") || (email == "") || (cpf == "") || (pis == "") || (country == "") || (state == "") || (city == "") || (number == "") || (complement == "")) {
+        alert("Preencha todos os campos!")
+      } else if (password != passwordConfirm) {
         alert("Senhas não conferem!");
+      } else {
+        alert("Registro Concluído");
+        this.router.navigate(['/login'])
       }
     }
+
 }
